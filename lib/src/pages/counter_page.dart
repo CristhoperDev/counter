2d32c159532bd27e0e50c744d9f2b420a414565d
Cristhoper Alvarez <acristhoper25@gmail.com>
2019-12-23 22:09:58 -0500

    Creating floating buttons

--- lib/src/pages/counter_page.dart
+++ lib/src/pages/counter_page.dart
@@ -12,28 +12,51 @@ class _CounterPageState extends State {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Stateful'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text('Number of taps:', style: _textStyle),
-            Text('$_count', style: _textStyle),
-          ],
+        appBar: AppBar(
+          title: Text('Stateful'),
+          centerTitle: true,
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {
-          //print('Hola mundo');
-          setState(() {
-            _count++;
-          });
-        },
-      ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text('Number of taps:', style: _textStyle),
+              Text('$_count', style: _textStyle),
+            ],
+          ),
+        ),
+        floatingActionButton: _CreateButtons());
+  }
+
+  Widget _CreateButtons() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: <Widget>[
+        SizedBox(width: 30),
+        FloatingActionButton(
+          child: Icon(Icons.exposure_zero),
+          onPressed: () {
+            setState(() {
+            });
+          },
+        ),
+        Expanded(child: SizedBox()),
+        FloatingActionButton(
+          child: Icon(Icons.remove),
+          onPressed: () {
+            setState(() {
+            });
+          },
+        ),
+        SizedBox(width: 5.0),
+        FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: () {
+            setState(() {
+            });
+          },
+        )
+      ],
     );
   }
 }
